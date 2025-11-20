b925bd5a4cdf44c287dd250fd7e702e101c550fa
alexbelz123 <belz.alexx@gmail.com>
2025-11-20 16:52:13 +0200

    updated portfolio styles

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -34,7 +34,7 @@ export default function Portfolio() {
           {t('portfolio.subtitle')}
         </p>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 max-w-[904px] gap-6 mx-auto">
           {portfolioCategories.map((category) => (
             <div
               key={category.slug}
